0b9c16654dfb463aed91a6d47ef373911333927e
kellerrennkadse <pr0@meetfurs.de>
2018-06-22 23:19:17 +0200

    Frontpage Port
    fixes #16
    Progresses #4

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,1 +1,8 @@
-console.log('Yeah')
+let langpicker = document.getElementsByClassName("language-picker")[0];
+let langMenuTrigger = langpicker.getElementsByClassName('lang-trigger')[0];
+let langMenu = langpicker.getElementsByTagName('menu')[0];
+
+langMenuTrigger.addEventListener('click', function() {
+    langpicker.classList.toggle('open'),
+    langMenu.classList.toggle('hidden');
+});
