795b5c163f090e379b98f8d6644afec9536b0b3e
Rennkadse <40218022+kellerrennkadse@users.noreply.github.com>
2018-06-23 20:29:06 +0200

    Quick & Dirty i18n (#27)
    * Quick & Dirty i18n
    
    Fixes #26
    
    * Remove unneeded regexp
    
    * Fix Spanish
    
    * Add alternate tags in header
    
    * Improve Langpicker

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import * as path    from "path";
 import * as express from "express";
 import * as favicon from "serve-favicon";
-//import * as i18n    from "i18n";
+import * as i18n    from "i18n";
 import * as cookieP from "cookie-parser";
 
 import log from "./util/logging";
@@ -12,13 +12,12 @@ const app = express();
 
 log.info("Started.");
 
-/*
 i18n.configure({
     directory: "./src/languages",
     cookie: config.server.cookieprefix + "lang",
-    defaultLocale: "en"
+    defaultLocale: "en",
+    queryParameter: "lang"
 });
-*/
 
 app.enable("trust proxy");
 
@@ -28,16 +27,32 @@ app.set("views", "./src/views");
 app.use(cookieP());
 app.use(express.static("./public"));
 app.use(favicon("./src/assets/favicon.png"));
-//app.use(i18n.init);
+app.use(i18n.init);
 app.set("port", config.server.port);
 
 app.locals.assets = require('../public/mix-manifest.json');
+
+/*
+    i18n stuff
+ */
+let availableLanguages = Object.keys(i18n.getCatalog());
+app.locals.languages = availableLanguages;
+
+app.use((req, res, next) => {
+    let currentLocale = i18n.getLocale(req);
+    app.locals.currentLanguage = currentLocale;
+    app.locals.currentRoute = req.path.replace(/^\/|\/$/g, '');
+
+    res.cookie(config.server.cookieprefix + "lang", currentLocale, {maxAge: 9000, httpOnly: true});
+
+    next();
+});
 
 //require("./routes/router")(app);
 
 app.get('/', (req, res) => {
     res.render('index', {foo: 'FOO'})
-})
+});
 
 app.listen(app.get("port"), function(err){
     log.info("Listening on port " + app.get("port") + "...");
