c21cb75342f43b415eb60c8d0a26eebda00b7951
kellerrennkadse <pr0@meetfurs.de>
2018-06-24 17:52:56 +0200

    Contact your MEP page
    Fixes #8

---
+++ src/assets/js/mep.ts
@@ -0,0 +1,21 @@
+import * as jQuery from 'jquery';
+
+let componentRoot = jQuery('.contact-your-mep');
+let gridRoot = componentRoot.find('.mep-grid');
+let mepItems = gridRoot.find('.mep-item');
+
+if(gridRoot.length > 0) {
+    gridRoot.find('.open-overlay').on('click', event => {
+       jQuery(event.currentTarget).closest('figure').toggleClass('open');
+    });
+    gridRoot.find('.close-overlay').on('click', event => {
+        jQuery(event.currentTarget).closest('figure').toggleClass('open');
+    });
+
+    componentRoot.find('select#country-sel').on('change', event => {
+        let selection = jQuery(event.currentTarget).val().toLowerCase();
+
+        mepItems.show();
+        mepItems.not(`.${selection}`).hide();
+    });
+}
