f43a685fc7314ee162a8a034997f41ebc4d9d9a0
Pac <public@pa0x43.me>
2018-06-25 15:53:56 +0200

    Move service init outside the route; pass momentJS to the frontend; render frontend even if the twitter service returns errors

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import * as favicon from "serve-favicon";
 import * as i18n    from "i18n";
 import * as cookieP from "cookie-parser";
 import * as fs from 'fs';
+import * as moment from 'moment'
 import TwitterService from './services/TwitterService'
 
 import log from "./util/logging";
@@ -51,12 +52,15 @@ app.use((req, res, next) => {
 });
 
 //require("./routes/router")(app);
+let twitter = new TwitterService(config.twitter);
 
 app.get('/', (req, res) => {
     // Get tweets by hashtag
-    let twitter = new TwitterService(config.twitter);
+    let currentLocale = i18n.getLocale(req);
     twitter.getTweet().then((tweets) => {
-        res.render('index', {"tweets": tweets.statuses});
+        res.render('index', {"tweets": tweets.statuses, "moment": moment, "currLang": currentLocale});
+    }).catch((err) => {
+        res.render('index');
     });
 });
 
