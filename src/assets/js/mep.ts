732dc0addebe14994663c633b6e6fcfb59325066
kellerrennkadse <pr0@meetfurs.de>
2018-06-26 16:31:28 +0200

    Nicer MEP List
    Remaining MEPs slide into position after changing the filter.
    I think this looks much nicer overall and less clunky

--- src/assets/js/mep.ts
+++ src/assets/js/mep.ts
@@ -1,10 +1,20 @@
 import * as jQuery from 'jquery';
+import * as Masonry from 'masonry-layout';
 
 let componentRoot = jQuery('.contact-your-mep');
 let gridRoot = componentRoot.find('.mep-grid');
 let mepItems = gridRoot.find('.mep-item');
+let masonry = null;
 
 if(gridRoot.length > 0) {
+
+    masonry = new Masonry('.mep-grid', {
+        itemSelector: '.mep-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+        gutter: 25,
+    });
+
     gridRoot.find('.open-overlay').on('click', event => {
        jQuery(event.currentTarget).closest('figure').toggleClass('open');
     });
@@ -18,5 +28,6 @@ if(gridRoot.length > 0) {
 
         mepItems.show();
         mepItems.not(`.${selection}`).hide();
+        masonry.layout();
     });
 }
