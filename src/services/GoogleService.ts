95a5f81fd1f6a25359602f52b36a884d1a444ec1
Pac <public@pa0x43.me>
2018-07-05 14:32:29 +0200

    Rework of the Google Service

--- src/services/GoogleService.ts
+++ src/services/GoogleService.ts
@@ -1,42 +1,31 @@
 import CacheService from "./CacheService";
-import * as GoogleSpreadsheet  from "google-spreadsheet";
-import conf from "../config";
+import * as google from "googleapis";
 
 class GoogleService {
 
     private cache: CacheService;
-    private doc: GoogleSpreadsheet;
-    private sheet;
     private config;
+    private sheets;
     constructor(config) {
         this.config = config;
-        this.cache = new CacheService(1200);
-        this.doc = new GoogleSpreadsheet(conf.google_sheet);
+        this.cache = new CacheService(this.config.ttl);
+        this.sheets = new google.sheets_v4.Sheets({
+            withCredentials: false,
+            auth: this.config.api_key
+        });
     }
 
     getData(): any {
         return this.cache.get("press_reviews", () => {
-                return new Promise((resolve, reject) => {
-                    this.doc.useServiceAccountAuth(this.config, (err1) => {
-                        if (err1) { reject(err1); }
-                        this.doc.getInfo((err2, info) => {
-                            if (err2) { reject(err2); }
-                            let last_update = info.updated;
-                            this.sheet = info.worksheets[0];
-                            this.sheet.getRows(( err3, rows ) => {
-                                if (err3) { reject(err3); }
-                                rows.shift(); // Remove the Table header
-                                rows.reverse(); // Reverse the Array so the newer rows are first
-                                resolve({
-                                    "rows": rows,
-                                    "last_update": last_update
-                                });
-                            });
-                        });
-                    });
-            }).then(value => {
-                return value;
-            });
+                return this.sheets.spreadsheets.values.get({
+                    spreadsheetId: this.config.sheet_id,
+                    range: this.config.range
+                }).then((res) => {
+                    res.data.values.reverse();
+                    return Promise.resolve(res.data);
+                });
+        }).then(value => {
+            return value;
         });
     }
 }
