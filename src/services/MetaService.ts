712f9085a74c9446ba6c85b8ed6309ce0f55859c
kellerrennkadse <pr0@meetfurs.de>
2018-07-08 22:43:38 +0200

    Add MetaData api endpoint

---
+++ src/services/MetaService.ts
@@ -0,0 +1,41 @@
+import CacheService from "./CacheService";
+import * as Crypto from 'crypto';
+
+const Metascraper = require('metascraper')
+const Got = require('got')
+
+class MetaService {
+
+    private cache: CacheService;
+    private config;
+
+    constructor(config) {
+        this.config = config;
+        this.cache = new CacheService(this.config.ttl);
+    }
+
+    get(page: string): any {
+        let cacheKey = this.config.prefix +  Crypto.createHash('md5').update(page).digest('hex');
+
+        return this.cache.get(cacheKey, async () => {
+            // Deny loopback
+            if(/^https?:\/\/localhost|^https?:\/\/127(?:\.[0-9]+){0,2}\.[0-9]+/.test(page)) {
+                return Promise.reject("Invalid hostname");
+            }
+
+            try {
+                const {body: html, url} = await Got(page);
+                const meta = await Metascraper({html, url});
+
+                return Promise.resolve(meta);
+            } catch (e) {
+                return Promise.reject({message: e.message});
+            }
+
+        }).then(value => {
+            return value;
+        });
+    }
+}
+
+export default MetaService;
