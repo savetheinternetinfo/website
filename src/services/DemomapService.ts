31f2e4298b785ec27b6c7951d4846bff652164ca
Jonas Breuer <j.breuer@fz-juelich.de>
2019-02-28 15:31:00 +0100

    HotFix for Dortmund demo spot (#253)
    * Text/demopage (#251)
    
    * Removed Demos
    
    * Added - in front of text for DE
    
    * Added colon to DE sentence
    
    * Renamed domate on EN
    
    * Added 3 Demos
    
    * Less Potsdam
    
    * Changed order
    
    * Added more Languages
    
    * Implement validation of GeoJSON (#226)
    
    This implements the validation of the GeoJSON file when the coordinates are requested. This will help prevent further errors copied into the file!
    
    * Fixed Dortmund

--- src/services/DemomapService.ts
+++ src/services/DemomapService.ts
@@ -1,6 +1,7 @@
 import CacheService from "./CacheService";
 import * as fs from "fs";
 import log from "../util/logging";
+import * as Joi from "joi";
 
 class DemomapService {
 
@@ -13,18 +14,35 @@ class DemomapService {
     }
 
     private getCoordsFromJSON(): any {
+        const geoJSONschema = Joi.object().keys({
+            type: Joi.string().valid("FeatureCollection"),
+            features: Joi.array().items(Joi.object().keys({
+                type: Joi.string().valid("Feature"),
+                properties: Joi.array().items(Joi.object().keys({
+                    name: Joi.string().lowercase(),
+                    value: Joi.any(),
+                })),
+                geometry: Joi.object().keys({
+                    type: Joi.string().valid("Point"),
+                    coordinates: Joi.array().ordered([
+                        Joi.number().min(-20037508.3427892).max(20037508.3427892),
+                        Joi.number().min(-20037508.3427892).max(20037508.3427892)
+                    ])  
+                })
+            }))
+        });
         return this.cache.get("demomapcoords", () => {
             return new Promise((resolve, reject) => {
                 fs.readFile(this.options.coordsfile, "utf8", (err, data) => {
                     if (err) {
                         reject(err);
                     } else {
-                        return resolve(JSON.parse(data));
+                        return resolve(geoJSONschema.validate(data));
                     }
                 });
               });
         }).then(value => {
-            return value;
+            return geoJSONschema.validate(value);
         }).catch((err) => {
             log.error(err);
         });
